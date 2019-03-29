d3882492e509bfa21f538a8516c673c87210359e
jifrivly <jifrivly@gmail.com>
2019-03-29 23:16:42 +0530

    registration form completed and form validation started

---
+++ Assignment_1/my.js
@@ -0,0 +1,21 @@
+function validate() {
+    var firstname = document.forms["reg_form"]["firstname"];
+    var lastname = document.forms["reg_form"]["lastname"];
+    var dob = document.forms["reg_form"]["dob"];
+    var state = document.forms["reg_form"]["state"];
+    var district = document.forms["reg_form"]["district"];
+    var place = document.forms["reg_form"]["place"];
+    var pincode = document.forms["reg_form"]["pincode"];
+    var address = document.forms["reg_form"]["address"];
+    var qualification = document.forms["reg_form"]["qualification"];
+    var guardian = document.forms["reg_form"]["guardian"];
+    var gender = document.forms["reg_form"]["gender"];
+    var blood_group = document.forms["reg_form"]["blood_group"];
+    var mobile = document.forms["reg_form"]["mobile"];
+    var email = document.forms["reg_form"]["email"];
+    var password = document.forms["reg_form"]["password"];
+    var confirm_password = document.forms["reg_form"]["confirm_password"];
+
+
+    console.log(firstname.value);
+}

687892e52fc3683ec8057913abd7f72ded4b6fab
jifrivly <jifrivly@gmail.com>
2019-03-30 17:52:59 +0530

    javascript form validation 80% applied

--- Assignment_1/my.js
+++ Assignment_1/my.js
@@ -1,21 +1,116 @@
 function validate() {
-    var firstname = document.forms["reg_form"]["firstname"];
-    var lastname = document.forms["reg_form"]["lastname"];
-    var dob = document.forms["reg_form"]["dob"];
-    var state = document.forms["reg_form"]["state"];
-    var district = document.forms["reg_form"]["district"];
-    var place = document.forms["reg_form"]["place"];
-    var pincode = document.forms["reg_form"]["pincode"];
-    var address = document.forms["reg_form"]["address"];
-    var qualification = document.forms["reg_form"]["qualification"];
-    var guardian = document.forms["reg_form"]["guardian"];
-    var gender = document.forms["reg_form"]["gender"];
-    var blood_group = document.forms["reg_form"]["blood_group"];
-    var mobile = document.forms["reg_form"]["mobile"];
-    var email = document.forms["reg_form"]["email"];
-    var password = document.forms["reg_form"]["password"];
-    var confirm_password = document.forms["reg_form"]["confirm_password"];
+    var firstname = document.getElementById('firstname')
+    // var firstname = document.forms["reg_form"]["firstname"];
+    var lastname = document.getElementById("lastname");
+    var dob = document.getElementById("dob");
+    var state = document.getElementById("state");
+    var district = document.getElementById("district");
+    var place = document.getElementById("place");
+    var pincode = document.getElementById("pincode");
+    var address = document.getElementById("address");
+    var qualification = document.getElementById("qualification");
+    var guardian = document.getElementById("guardian");
+    var gender = document.getElementsByName("gender");
+    var blood_group = document.getElementById("blood_group");
+    var mobile = document.getElementById("mobile");
+    var email = document.getElementById("email")
+    // var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var confirm_password = document.getElementById("confirm_password");
 
 
-    console.log(firstname.value);
+    console.log(gender)
+
+    if (inputCheck.call(firstname) &&
+        inputCheck.call(dob) &&
+        inputCheck.call(state) &&
+        inputCheck.call(district) &&
+        inputCheck.call(place) &&
+        inputCheck.call(pincode) &&
+        inputCheck.call(address) &&
+        inputCheck.call(qualification) &&
+        inputCheck.call(guardian) &&
+        radioInputCheck.call(gender) &&
+        inputCheck.call(blood_group) &&
+        phoneInputCheck.call(mobile) &&
+        inputCheck.call(email) &&
+        inputCheck.call(password) &&
+        inputCheck.call(confirm_password)) {
+        console.log("Successfully checked")
+    }
+
+}
+
+// function to check normal inputs
+function inputCheck() {
+    if (this.value == "" | this.value == " ") {
+        requiredOutput.call(this);
+        return false;
+    } else {
+        successOutput.call(this)
+        return true;
+    }
+}
+
+
+// function to check radio button input 
+function radioInputCheck() {
+    // console.log("checking radio input..")
+    var option = ""
+    for (let i = 0; i < this.length; i++) {
+        // console.log("checking radio input.." + i)
+        console.log(this[i])
+        if (this[i].checked) {
+            option = this[i].value;
+            successOutput.call(this[this.length - 1].parentNode.parentNode)
+            return option
+        }
+    }
+    if (option == "") {
+        requiredOutput.call(this[this.length - 1].parentNode.parentNode)
+        return false
+    }
+}
+
+// function to check mobile number input
+function phoneInputCheck() {
+    if (this.value == "" | this.value == " ") {
+        requiredOutput.call(this);
+        return false;
+    } else if (this.value.length < 10) {
+        requiredOutput.call(this, " * 10 digits required.. ")
+    } else if (this.value.length > 10) {
+        requiredOutput.call(this, " * maximum 10 digits allowed.. ")
+    } else if (["9", "8", "7", "6"].indexOf(this.value.charAt(0)) == -1) {
+        console.log("1st digit checking started")
+        requiredOutput.call(this, " * Not a valid number.. ")
+    } else {
+        successOutput.call(this)
+    }
+    console.log(this.value.charAt(0) + ["9", "8", "7", "6"].indexOf(this.value.charAt(0)))
+
+}
+
+
+function requiredOutput(msg = " * This field is required") {
+    // console.log(this)
+    // console.log(this.nextElementSibling)
+    if (this.nextElementSibling.classList != "") {
+        // console.log("hiii classes removed and text-danger added")
+        this.nextElementSibling.classList = ""
+    }
+    this.nextElementSibling.classList.add("text-danger")
+    this.nextElementSibling.innerHTML = msg
+    this.focus()
+}
+
+function successOutput() {
+    // console.log(this)
+    // console.log(this.nextElementSibling)
+    if (this.nextElementSibling.classList != "") {
+        // console.log("hiii classes removed and text-success added")
+        this.nextElementSibling.classList = ""
+    }
+    this.nextElementSibling.classList.add("text-success")
+    this.nextElementSibling.innerHTML = '<i class="fa fa-check fa-lg"></i> Input success.';
 }

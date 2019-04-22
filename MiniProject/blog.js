845031a0da94a8bb1ff01bfc079c7fc50d246365
jifrivly <jifrivly@gmail.com>
2019-04-22 13:06:59 -0400

    mongoDB server ready mongoose added

--- MiniProject/blog.js
+++ MiniProject/blog.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 var blog = express.Router()
 
+blog.use(bodyParser.urlencoded())
 
 blog.get("/", (req, res) => {
     res.render("under-construction")
@@ -13,6 +15,18 @@ blog.get("/details", (req, res) => {
 
 blog.get("/login", (req, res) => {
     res.render("login")
+})
+
+blog.post("/login",(req,res)=>{
+    var user = req.body.username
+    var pass = req.body.password
+    if (user == "" | pass == "") {
+        res.send("fields must not be null")
+    } else if (user == "jifrivly" && pass == "123456") {
+        res.send(user + "is Successfully logged in")
+    } else {
+        res.send("username or password not match")
+    }
 })
 
 blog.get("/create", (req, res) => {
